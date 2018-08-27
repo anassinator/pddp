Fix BNN gradients

pddp/controllers/pddp.py
@@ -24,8 +24,8 @@ with warnings.catch_warnings():
 
 from torch.utils.data import TensorDataset
 
-from .ilqr import (iLQRController, forward, backward, Q, _control_law,
-                   _trajectory_cost)
+from .ilqr import (iLQRController, forward, backward, Q, _linear_control_law,
+                   _control_law, _trajectory_cost)
 
 from ..utils.encoding import StateEncoding, decode_var
 from ..utils.evaluation import eval_cost, eval_dynamics
@@ -76,6 +76,7 @@ class PDDPController(iLQRController):
             max_reg=1e10,
             quiet=False,
             on_iteration=None,
+            linearize_dynamics=False,
             max_var=1.0,
             n_sample_trajectories=4,
             train_on_start=True,
@@ -98,6 +99,8 @@ class PDDPController(iLQRController):
                     J_opt (Tensor<0>): Iteration's total cost to-go.
                     accepted (bool): Whether the iteration was accepted or not.
                     converged (bool): Whether the iteration converged or not.
+            linearize_dynamics (bool): Whether to linearize the dynamics when
+                computing the control law.
             max_var (Tensor<0>): Maximum variance allowed before resampling
                 trajectories.
             n_sample_trajectories (int): Number of trajectories to sample from
@@ -153,8 +156,14 @@ class PDDPController(iLQRController):
 
                 # Backtracking line search.
                 for alpha in alphas:
-                    Z_new, U_new = _control_law(self._model, Z, U, k, K, alpha,
-                                                encoding, self._model_opts)
+                    if linearize_dynamics:
+                        Z_new, U_new = _linear_control_law(
+                            Z, U, F_z, F_u, k, K, alpha)
+                    else:
+                        Z_new, U_new = _control_law(self._model, Z, U, k, K,
+                                                    alpha, encoding,
+                                                    self._model_opts)
+
                     J_new = _trajectory_cost(self._cost, Z_new, U_new, encoding,
                                              self._cost_opts)
 
@@ -233,3 +242,5 @@ def _sample(env, Us, quiet=False):
             dX[j] = x_next - x
 
         env.reset()
+
+    return TensorDataset(X_, dX)

pddp/models/bnn.py
@@ -144,10 +144,11 @@ def bnn_dynamics_model_factory(state_size, action_size, hidden_features,
                     (Tensor<n_particles, ..., state_size>).
             """
             # Moment match.
+            # Sample manually to preserve gradients.
             mean = decode_mean(z, encoding)
             std = decode_std(z, encoding)
-            dist = torch.distributions.Normal(mean, std)
-            x = dist.sample(torch.Size([self.n_particles]))
+            x = mean.expand(self.n_particles, *mean.shape)
+            x = x + std * torch.randn_like(x)
 
             u_ = u.expand(self.n_particles, *u.shape)
             x_ = torch.cat([x, u_], dim=-1)
@@ -234,7 +235,7 @@ class BDropout(torch.nn.Dropout):
         self.p = 1 - self.rate
         self.noise.data = torch.bernoulli(self.p.expand(x.shape))
 
-    def forward(self, x, resample=True, mask_dims=2, **kwargs):
+    def forward(self, x, resample=True, mask_dims=None, **kwargs):
         """Computes the binary dropout.
 
         Args:
@@ -244,6 +245,9 @@ class BDropout(torch.nn.Dropout):
         Returns:
             Output (Tensor).
         """
+        if mask_dims is None:
+            mask_dims = x.dim() - 1
+
         sample_shape = x.shape[-mask_dims:]
         if sample_shape != self.noise.shape:
             sample = x.view(-1, *sample_shape)[0]
@@ -338,52 +342,6 @@ class CDropout(BDropout):
             Module representation (str).
         """
         return "temperature={}".format(self.temperature)
-
-
-class TLNDropout(BDropout):
-
-    """Truncated log-normal dropout (NIPS 2017)."""
-
-    def __init__(self, interval=[-10, 0]):
-        super(TLNDropout, self).__init__()
-        self.register_buffer("interval", torch.tensor(interval))
-        self.logit_posterior_mean = Parameter(
-            -torch.log(1.0 / torch.tensor(1 - self.rate) - 1.0))
-        self.logit_posterior_std = logit_posterior_std
-
-    def regularization(self, weight, bias):
-        """Computes the regularization cost.
-
-        Args:
-            weight (Tensor): Weight tensor.
-            bias (Tensor): Bias tensor.
-
-        Returns:
-            Regularization cost (Tensor).
-        """
-        # Ignore weight regularization as their independent here.
-        bias_reg = (bias**2).sum() if bias is not None else 0
-        return self.reg * (weight_reg + bias_reg)
-
-    def _update_noise(self, x):
-        """Updates the dropout noise.
-
-        Args:
-            x (Tensor): Input.
-        """
-        pass
-
-    def forward(self, x, resample=True, mask_dims=2, **kwargs):
-        """Computes the binary dropout.
-
-        Args:
-            x (Tensor): Input.
-            resample (bool): Whether to force resample.
-
-        Returns:
-            Output (Tensor).
-        """
-        pass
 
 
 class BSequential(torch.nn.Sequential):

pddp/utils/autodiff.py
@@ -17,7 +17,7 @@
 import torch
 
 
-def grad(y, x, **kwargs):
+def grad(y, x, allow_unused=True, **kwargs):
     """Evaluates the gradient of y w.r.t x safely.
 
     Args:
@@ -30,7 +30,7 @@ def grad(y, x, **kwargs):
         Gradient (Tensor<n>).
     """
     dy_dx, = torch.autograd.grad(
-        y, x, retain_graph=True, allow_unused=True, **kwargs)
+        y, x, retain_graph=True, allow_unused=allow_unused, **kwargs)
 
     # The gradient is None if disconnected.
     dy_dx = dy_dx if dy_dx is not None else torch.zeros_like(x)

tests/utils/test_encoding.py
@@ -177,6 +177,9 @@ def test_decode_mean_covar_var_std(encoding):
     # Verify differentiable.
     allow_unused = encoding == StateEncoding.IGNORE_UNCERTAINTY
     for i in range(N):
+        costs = mean, var, std
+        wrts = original_mean, original_covar, original_covar
+
         for j in range(M):
             for k in range(M):
                 torch.autograd.grad(
@@ -185,8 +188,6 @@ def test_decode_mean_covar_var_std(encoding):
                     allow_unused=allow_unused,
                     retain_graph=True)
 
-            costs = mean, var, std
-            wrts = original_mean, original_covar, original_covar
             for cost, wrt in zip(costs, wrts):
                 torch.autograd.grad(
                     cost[i, j],