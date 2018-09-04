compute the full covariance matrix of predictions, fix memeory leak

pddp/models/bnn/modules.py
@@ -189,7 +189,7 @@ def bnn_dynamics_model_factory(state_size,
                     resample=False,
                     return_samples=False,
                     sample_input_distribution=True,
-                    use_predicted_std=True,
+                    use_predicted_std=False,
                     **kwargs):
             """Dynamics model function.
 
@@ -282,6 +282,18 @@ def bnn_dynamics_model_factory(state_size,
                 return x + dx
 
             M = mean + dx.mean(dim=0)
+            if encoding in [StateEncoding.FULL_COVARIANCE_MATRIX,
+                            StateEncoding.UPPER_TRIANGULAR_CHOLESKY]:
+                deltas = dx - dx.mean()
+                jitter = 1e-9*torch.eye(
+                    dx.shape[-1], device=dx.device, dtype=dx.dtype)
+                if deltas.dim() == 3:
+                    deltas = deltas.permute(1, 0, 2)
+                    C = deltas.transpose(1, 2).bmm(deltas)/dx.shape[0] + jitter
+                else:
+                    C = deltas.t().mm(deltas)/dx.shape[0] + jitter
+                return encode(M, C=C, encoding=encoding)
+
             S = dx.std(dim=0)
             return encode(M, S=S, encoding=encoding)
 

pddp/utils/encoding.py
@@ -134,10 +134,7 @@ def encode(M, C=None, V=None, S=None, encoding=StateEncoding.DEFAULT):
         if C.dim() == 2:
             L = C.potrf()
         elif C.dim() == 3:
-            # TODO: Remove for-loop.
-            L = torch.zeros_like(C, requires_grad=M.requires_grad)
-            for c, l in zip(C, L):
-                l += c.potrf()
+            L = torch.stack([c.potrf() for c in C])
         else:
             raise NotImplementedError("Expected a 2D or 3D tensor")
 