Fix BNN variance estimate with default encoding

pddp/models/bnn/modules.py
@@ -233,7 +233,7 @@ def bnn_dynamics_model_factory(state_size,
                     eps = eps.unsqueeze(1).repeat(1, x.shape[1], 1)
                     x = x + (eps[:, :, :, None] * L[None, :, :, :]).sum(-2)
                 else:
-                    x = x + eps * std
+                    x = x + eps.mm(L)
 
             if angular:
                 x_ = augment_state(x, angular_indices, non_angular_indices)
@@ -284,8 +284,8 @@ def bnn_dynamics_model_factory(state_size,
             if encoding in (StateEncoding.FULL_COVARIANCE_MATRIX,
                             StateEncoding.UPPER_TRIANGULAR_CHOLESKY):
                 # Compute full covariance matrix when needed.
-                deltas = dx - dx.mean(dim=0)
-                jitter = 1e-9
+                deltas = output - output.mean(dim=0)
+                jitter = 1e-12
                 ret = None
                 while jitter < 10:
                     try:

pddp/utils/encoding.py
@@ -340,7 +340,7 @@ def decode_covar_sqrt(Z, encoding=StateEncoding.DEFAULT, state_size=None):
         return L
     elif encoding == StateEncoding.VARIANCE_ONLY:
         if other.dim() == 1:
-            return other.diag()
+            return other.diag().sqrt()
         elif other.dim() == 2:
             # TODO: Remove for-loop.
             return torch.stack([x.diag().sqrt() for x in other])
@@ -348,7 +348,7 @@ def decode_covar_sqrt(Z, encoding=StateEncoding.DEFAULT, state_size=None):
             raise NotImplementedError("Expected a 1D or 2D tensor")
     elif encoding == StateEncoding.STANDARD_DEVIATION_ONLY:
         if other.dim() == 1:
-            return other.diag().pow(2)
+            return other.diag()
         elif other.dim() == 2:
             # TODO: Remove for-loop.
             return torch.stack([x.diag() for x in other])